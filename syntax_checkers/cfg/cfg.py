b7f96a32e9ad3235f90248adc5ac16977edc1b91
Steven Myint <git@stevenmyint.com>
2014-04-19 12:06:09 -0700

    Add "cfg" syntax checker

---
+++ syntax_checkers/cfg/cfg.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+try:
+    from configparser import ConfigParser
+    import configparser
+except ImportError:
+    from ConfigParser import SafeConfigParser as ConfigParser
+    import ConfigParser as configparser
+
+import re
+import sys
+
+
+if len(sys.argv) != 2:
+    raise SystemExit('usage: cfg_check.py filename')
+
+parser = ConfigParser()
+
+# Use ugly syntax to support very old versions of Python.
+try:
+    parser.read(sys.argv[1])
+except configparser.ParsingError:
+    error = sys.exc_info()[1]
+
+    # "error.lineno" is not populated in this case.
+    line_number = 0
+    found = re.search(r'\[line  ([0-9]+)\]', error.message)
+    if found:
+        line_number = int(found.group(1))
+
+    sys.stderr.write('%s:%s: %s\n' %
+                     (error.source, line_number, error.message))
+except configparser.Error:
+    error = sys.exc_info()[1]
+    sys.stderr.write('%s:%s: %s\n' %
+                     (error.source, error.lineno, error.message))
---
+++ syntax_checkers/cfg/cfg.vim
@@ -0,0 +1,32 @@
+if exists("g:loaded_syntastic_cfg_cfg_checker")
+    finish
+endif
+let g:loaded_syntastic_cfg_cfg_checker = 1
+
+let s:save_cpo = &cpo
+set cpo&vim
+
+let s:checker = expand('<sfile>:p:h') . syntastic#util#Slash() . 'cfg.py'
+
+function! SyntaxCheckers_cfg_cfg_IsAvailable() dict
+    return executable(self.getExec()) &&
+        \ syntastic#util#versionIsAtLeast(syntastic#util#getVersion(self.getExecEscaped() . ' -V'), [2, 4])
+endfunction
+
+function! SyntaxCheckers_cfg_cfg_GetLocList() dict
+    let makeprg = self.makeprgBuild({'exe': [self.getExec(), s:checker]})
+
+    return SyntasticMake({
+        \ 'makeprg': makeprg,
+        \ 'errorformat':
+        \     '%f:%l: %m',
+        \ 'returns': [0]})
+endfunction
+
+call g:SyntasticRegistry.CreateAndRegisterChecker({
+    \ 'filetype': 'cfg',
+    \ 'exec': 'python',
+    \ 'name': 'cfg'})
+
+let &cpo = s:save_cpo
+unlet s:save_cpo
