d485cbf731d025026e676a6884052c8e5c09fa64
Steven Myint <git@stevenmyint.com>
2014-04-27 09:17:01 -0700

    Ignore check if module is not available

--- syntax_checkers/yaml/check_yaml.py
+++ syntax_checkers/yaml/check_yaml.py
@@ -4,26 +4,35 @@
 
 import sys
 
-import yaml
 
-
-if len(sys.argv) != 2:
-    raise SystemExit('usage: %s filename' % (sys.argv[0],))
-filename = sys.argv[1]
-
-# Use ugly syntax to support very old versions of Python.
-try:
+def main():
     try:
-        input_file = open(filename, 'rb')
-        yaml.load(input_file)
-    finally:
-        input_file.close()
-except yaml.error.YAMLError:
-    error = sys.exc_info()[1]
-    mark = error.problem_mark
+        import yaml
+    except ImportError:
+        # Don't do anything if yaml module is not available.
+        return 0
 
-    sys.stderr.write('%s:%s:%s: %s\n' %
-                     (filename,
-                      mark.line + 1,
-                      mark.column + 1,
-                      error.problem))
+    if len(sys.argv) != 2:
+        raise SystemExit('usage: %s filename' % (sys.argv[0],))
+    filename = sys.argv[1]
+
+    # Use ugly syntax to support very old versions of Python.
+    try:
+        try:
+            input_file = open(filename, 'rb')
+            yaml.load(input_file)
+        finally:
+            input_file.close()
+    except yaml.error.YAMLError:
+        error = sys.exc_info()[1]
+        mark = error.problem_mark
+
+        sys.stderr.write('%s:%s:%s: %s\n' %
+                         (filename,
+                          mark.line + 1,
+                          mark.column + 1,
+                          error.problem))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
