afc64d885fbe725dd6606cc1e82def046d95cdc2
Steven Myint <git@stevenmyint.com>
2014-04-27 13:16:19 -0700

    Clean up

--- syntax_checkers/cfg/cfg.py
+++ syntax_checkers/cfg/cfg.py
@@ -11,26 +11,31 @@ import re
 import sys
 
 
-if len(sys.argv) != 2:
-    raise SystemExit('usage: %s filename' % (sys.argv[0],))
-filename = sys.argv[1]
+def main():
+    if len(sys.argv) != 2:
+        raise SystemExit('usage: %s filename' % (sys.argv[0],))
+    filename = sys.argv[1]
 
-parser = ConfigParser()
+    parser = ConfigParser()
 
-# Use ugly syntax to support very old versions of Python.
-try:
-    parser.read(filename)
-except configparser.MissingSectionHeaderError:
-    # Ignore non-standard cfg files.
-    pass
-except configparser.Error:
-    error = sys.exc_info()[1]
+    # Use ugly syntax to support very old versions of Python.
+    try:
+        parser.read(filename)
+    except configparser.MissingSectionHeaderError:
+        # Ignore non-standard cfg files.
+        pass
+    except configparser.Error:
+        error = sys.exc_info()[1]
 
-    line_number = getattr(error, 'lineno', 0)
-    if not line_number:
-        found = re.search(r'\[line\s+([0-9]+)\]', error.message)
-        if found:
-            line_number = int(found.group(1))
+        line_number = getattr(error, 'lineno', 0)
+        if not line_number:
+            found = re.search(r'\[line\s+([0-9]+)\]', error.message)
+            if found:
+                line_number = int(found.group(1))
 
-    sys.stderr.write('%s:%s: %s\n' %
-                     (filename, line_number, error.message))
+        sys.stderr.write('%s:%s: %s\n' %
+                         (filename, line_number, error.message))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
