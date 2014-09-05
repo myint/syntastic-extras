fd7a360d6a2cb47bc204f10a8e987fd48844d3e9
Steven Myint <git@stevenmyint.com>
2014-09-05 06:58:12 -0700

    Only check if there is a ".syntastic_cpp_config"

--- syntax_checkers/cpp/check.py
+++ syntax_checkers/cpp/check.py
@@ -2,8 +2,47 @@
 
 """Check C++ file syntax."""
 
+import os
 import subprocess
 import sys
+
+
+def find_configuration(start_path):
+    """Return path to configuration.
+
+    Return None if there is no configuration.
+
+    """
+    while start_path:
+        start_path = os.path.dirname(start_path)
+
+        configuration_path = os.path.join(start_path, '.syntastic_cpp_config')
+        if os.path.exists(configuration_path):
+            return configuration_path
+
+        if start_path == os.sep:
+            break
+
+    return None
+
+
+def read_configuration(start_path):
+    """Return compiler options from configuration.
+
+    Return None if there is no configuration.
+
+    """
+    configuration_path = find_configuration(os.path.abspath(start_path))
+    if configuration_path:
+        input_file = None
+        try:
+            input_file = open(configuration_path)
+            return input_file.read().splitlines()
+        finally:
+            if input_file:
+                input_file.close()
+    else:
+        return None
 
 
 def main():
@@ -12,7 +51,12 @@ def main():
     compiler = sys.argv[1]
     filename = sys.argv[2]
 
-    return subprocess.call([compiler, '-fsyntax-only', filename])
+    options = read_configuration(filename)
+
+    if options is None:
+        return 0
+
+    return subprocess.call([compiler, '-fsyntax-only'] + options + [filename])
 
 
 if __name__ == '__main__':
