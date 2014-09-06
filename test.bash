45627273177296576b059f44d92233a9382d7993
Steven Myint <git@stevenmyint.com>
2014-09-06 08:00:20 -0700

    Make error checking more specific

--- test.bash
+++ test.bash
@@ -1,28 +1,22 @@
 #!/bin/bash -eux
 
+trap "echo -e '\x1b[01;31mFailed\x1b[0m'" ERR
+
 ./syntax_checkers/cfg/cfg.py test/good.cfg
 ./syntax_checkers/c/check.py .syntastic_c_config gcc test/good.c
 ./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ test/good.cpp
 ./syntax_checkers/yaml/check_yaml.py test/good.yaml
 
-if ./syntax_checkers/cfg/cfg.py test/bad.cfg
-then
-    exit 1
-fi
+./syntax_checkers/cfg/cfg.py test/bad.cfg 2>&1 \
+    | grep 'test/bad.cfg:2' > /dev/null
 
-if ./syntax_checkers/c/check.py .syntastic_c_config gcc test/bad.c
-then
-    exit 1
-fi
+./syntax_checkers/c/check.py .syntastic_c_config gcc test/bad.c 2>&1 \
+    | grep 'test/bad.c:3' > /dev/null
 
-if ./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ test/bad.cpp
-then
-    exit 1
-fi
+./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ test/bad.cpp 2>&1 \
+    | grep 'test/bad.cpp:3' > /dev/null
 
-if ./syntax_checkers/yaml/check_yaml.py test/bad.yaml
-then
-    exit 1
-fi
+./syntax_checkers/yaml/check_yaml.py test/bad.yaml 2>&1 \
+    | grep 'test/bad.yaml:2'
 
 echo -e '\x1b[01;32mOkay\x1b[0m'
