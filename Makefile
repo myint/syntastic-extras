28d047676f35cad6bf02b04494a4a67f5b1fdcd6
Steven Myint <git@stevenmyint.com>
2014-09-20 16:07:10 -0700

    Test on Python 2.4

---
+++ Makefile
@@ -0,0 +1,5 @@
+test:
+	PYTHON=python2.4 ./test.bash
+	PYTHON=python3.4 ./test.bash
+
+.PHONY: test
--- test.bash
+++ test.bash
@@ -1,24 +1,32 @@
-#!/bin/bash -eux
+#!/bin/bash -ex
+
+if [[ -z "$PYTHON" ]]
+then
+    PYTHON=python
+fi
 
 trap "echo -e '\x1b[01;31mFailed\x1b[0m'" ERR
 
-./syntax_checkers/cfg/cfg.py test/good.cfg
-./syntax_checkers/c/check.py .syntastic_c_config gcc test/good.c
-./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ test/good.cpp
-./syntax_checkers/cpp/check.py .syntastic_c_config gcc -x c test/foo/bar.h
-./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ -x c++ test/foo/bar.h
-./syntax_checkers/yaml/check_yaml.py test/good.yaml
+"$PYTHON" ./syntax_checkers/cfg/cfg.py test/good.cfg
+"$PYTHON" ./syntax_checkers/c/check.py .syntastic_c_config gcc test/good.c
+"$PYTHON" ./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ test/good.cpp
+"$PYTHON" ./syntax_checkers/cpp/check.py .syntastic_c_config gcc -x c test/foo/bar.h
+"$PYTHON" ./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ -x c++ test/foo/bar.h
 
-./syntax_checkers/cfg/cfg.py test/bad.cfg 2>&1 \
+"$PYTHON" ./syntax_checkers/cfg/cfg.py test/bad.cfg 2>&1 \
     | grep 'test/bad.cfg:2' > /dev/null
 
-./syntax_checkers/c/check.py .syntastic_c_config gcc test/bad.c 2>&1 \
+"$PYTHON" ./syntax_checkers/c/check.py .syntastic_c_config gcc test/bad.c 2>&1 \
     | grep 'test/bad.c:3' > /dev/null
 
-./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ test/bad.cpp 2>&1 \
+"$PYTHON" ./syntax_checkers/cpp/check.py .syntastic_cpp_config g++ test/bad.cpp 2>&1 \
     | grep 'test/bad.cpp:3' > /dev/null
 
-./syntax_checkers/yaml/check_yaml.py test/bad.yaml 2>&1 \
-    | grep 'test/bad.yaml:2'
+if "$PYTHON" -c 'import yaml' >& /dev/null
+then
+    "$PYTHON" ./syntax_checkers/yaml/check_yaml.py test/good.yaml
+    "$PYTHON" ./syntax_checkers/yaml/check_yaml.py test/bad.yaml 2>&1 \
+        | grep 'test/bad.yaml:2'
+fi
 
 echo -e '\x1b[01;32mOkay\x1b[0m'
